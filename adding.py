a18dce1314b246701698d2959e0969e1bde57fe3
rickp1011 <92723258+rickp1011@users.noreply.github.com>
2022-03-28 15:43:28 +0800

    Add files via upload

---
+++ adding.py
@@ -0,0 +1,4 @@
+x = int(input("num1"))
+y = int(input("num 2"))
+sum = (x + y)/2
+print(sum)
---
+++ lab1 using 2 and 3.py
@@ -0,0 +1,11 @@
+code = input("module code")
+if code == "csc 1009":
+    print("csc1009")
+elif code =="csc1010":
+    print("csc 1010")
+else:
+    print("tryagain")
+#q3
+for x in range (102,66,-1):
+    if (x%2!=0):
+        print(x)
